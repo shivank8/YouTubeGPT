4a943a47107557df565adb8f0ad78f84257f609c
Shivank8 <shivankverma890@gmail.com>
2023-08-14 20:18:35 +0530

    Added chat-activity with recycler view

---
+++ app/src/main/java/com/example/huggingface/ChatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.huggingface
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
